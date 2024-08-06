6ebdf1cd13aaeccad3d4a975db7d41d316712db0
VictorJGB <victorgb.dev@gmail.com>
2024-08-06 16:53:26 -0300

    feat: Home layout

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, type OnInit } from '@angular/core';
 
+// material
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 @Component({
   selector: 'home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatChipsModule, MatTooltipModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
